77c171b262cc8fdf4a82c48064cbff7323fb5434
PrasadAmbokar <58807897+PrasadAmbokar@users.noreply.github.com>
2021-01-07 18:06:28 +0530

    Encapsulation

---
+++ OOPs/Encapsulation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.Encapsulation
+{
+    class Encapsulation
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Prasad";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
---
+++ OOPs/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.Encapsulation
+{
+    class Person
+    {
+        private string name;  
+        public string Name    
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
