12d024e8e36264b69b565a8e7837ded3cd6ea353
PrasadAmbokar <58807897+PrasadAmbokar@users.noreply.github.com>
2021-01-07 18:04:57 +0530

    Add files via upload

---
+++ Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void MyMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + " is " + age);
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod("Prasad", 23);
+            MyMethod("Omkar", 25);
+            MyMethod("Ravina", 22);
+        }
+    }
+}
