6cce41149a3c3c55d414a31cf19f802e017e7fc9
PrasadAmbokar <58807897+PrasadAmbokar@users.noreply.github.com>
2021-01-07 18:01:56 +0530

    Interface

---
+++ ExceptionHandling/EnumApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class EnumApp
+    {
+        enum Months
+        {
+            January,    
+            February,   
+            March,      
+            April                  
+        }
+        static void Main(string[] args)
+        {
+            int myNum = (int)Months.April;
+            Console.WriteLine(myNum);
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApplication
+{
+    interface IAnimal
+    {
+        void animalSound(); 
+    }
+    class Tiger : IAnimal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("Tiger");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiger myPig = new Tiger();  
+            myPig.animalSound();
+        }
+    }
+}
