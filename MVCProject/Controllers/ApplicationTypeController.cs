266a266a8c7375ade0ffc05aa0e359abdc4271c3
PrasadAmbokar <58807897+PrasadAmbokar@users.noreply.github.com>
2021-01-08 19:23:02 +0530

    Add files via upload

---
+++ MVCProject/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Data;
+using MVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApp.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
+            return View(objList);
+
+        }
+
+        public IActionResult create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult create(ApplicationType obj)
+        {
+            _db.ApplicationType.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVCProject/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Data;
+using MVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        //GET-create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post-create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
